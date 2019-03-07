82f3a8e9e82549919d1ad53a4c9a566b1c003670
Jon Corbin <jon.corbin8@gmail.com>
2019-03-07 13:41:41 -0500

    Refactor and fix errors

--- Crawler.py
+++ Crawler.py
@@ -1,79 +1,106 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 import requests
 import bs4 as bs #BeautifulSoup
 import string
 import random
+from collections import Counter
 
-#Worker
-def getLinks(url):
-    links = []
-    print("Working...")
-    try:
-        page = requests.get(url)
-        soup = bs.BeautifulSoup(page.text, 'lxml')
-        for link in soup.find_all('a'):
-            link = link.get('href')
-            #if there are no links on the page
-            if link == None:
-                continue
-            #add the wikipedia link to the front of relative links
-            if link[0] == "/":
-                link = "https://en.wikipedia.org" + link
-            #don't care for anchor links
-            elif "#" in link:
-                continue
+class Crawler:
+    def __init__(self, saveFileName, cycles):
+        self.saveFileName = saveFileName
+        self.cycles = cycles
+        self.totalLinkCount = 0
 
-            links.append(link)
-        print("Links for " + url + " collected")
-        return links
-    #Catch connection errors if link does not exist
-    except ConnectionError as e:
-        print(e)
-        print("Connection Error to " + url)
-        return []
-    #Catch the rest of exceptions in a rather unfavorable way
-    except Exception as e:
-        print(e)
-        return []
+    #Worker
+    def getLinks(self, url):
+        baseUrl = "/".join(url.split("/")[:3])
+        links = []
+        print("Collecting links from {}".format(url))
+        try:
+            page = requests.get(url)
+            soup = bs.BeautifulSoup(page.text, 'lxml')
+            for link in soup.find_all('a'):
+                linkRef = link.get('href')
+                #if there are no links on the page
+                if linkRef == None or linkRef == "" or "#" in linkRef:
+                    continue
+                #add the base link to the front of relative links
+                if linkRef[0] == "/":
+                    if len(linkRef) > 1 and linkRef[1] == "/":
+                        linkRef = "https:" + linkRef
+                    else:
+                        linkRef = baseUrl + linkRef
 
+                links.append(linkRef)
 
-###Unused but optional use to generate an amount amt of random links of length length
-def generateLinks(amt, length):
-    link_ls = []
-    com = ".com"
-    for i in range(amt):
-        link = "http://www."
-        for j in range(length):
-            link += random.choice(string.ascii_lowercase)
-        link += com
-        link_ls.append(link)
-    return link_ls
+            print("{} links collected from {}".format(len(links), url))
+            return links
 
+        #Catch connection errors if url does not exist
+        except ConnectionError as e:
+            print(e)
+            print("Connection Error to " + url)
+            return []
+        #Catch the rest of exceptions in a rather unfavorable way
+        except Exception as e:
+            raise e
 
-def main(link_ls, cycles):
-    for i in range():
-        #links = generateLinks(50, 3)
-        p = Pool(4)
-        print("Start Pool"+str(i+1))
-        link_ls = p.map(getLinks, link_ls)
+    ###Unused but optional use to generate an amount (amt) of random links of length (length)
+    def generateLinks(self, amt, length):
+        print("Generating {} links of {} length")
+        link_ls = []
+        for _ in range(amt):
+            link = "https://www."
+            for _ in range(length):
+                link += random.choice(string.ascii_lowercase)
+            link += ".com"
+            link_ls.append(link)
+        return link_ls
 
-        link_ls_temp = []
+    def writeLinks(self, linkList):
         #Write links to file
-        if len(link_ls2) > 0 and link_ls2[0] != None:
-            with open("links.txt", "a", encoding="utf_8") as f:
-                for links in link_ls:
+        if len(linkList) > 0 and linkList[0] != None:
+            flattened = []
+            with open(self.saveFileName, "w", encoding="utf_8") as f:
+                for links in linkList:
                     for link in links:
                         if link == None:
                             continue
                         f.write(link + "\n")
-                        #add link to the new list to run through
-                        link_ls_temp.append(link)
+                        flattened.append(link)
+                        self.totalLinkCount += 1
+                linkList = flattened
         else:
             print("No links collected")
+
+        return linkList
+        
+
+    def printSummary(self):
+        print("\n" + "#"*75)
+        print("{} total links found after {} cycles.".format(self.totalLinkCount, self.cycles))
+        
+        with open(self.saveFileName, "r") as f:
+            counter = Counter(f.readlines())
+
+        print("Most Common links found:\nCount|Link\n{}".format("\n".join("{}| {}".format(count, link.replace("\n", "")) for link, count in counter.most_common(5))))
+        print("#"*75)
+
+
+    def crawl(self, linkList):
+        with Pool(cpu_count()-1) as p:
+            for i in range(self.cycles):
+                print("\nStart Pool #"+str(i+1))
+                linkList = p.map(self.getLinks, linkList)
+
+                linkList = self.writeLinks(linkList)
+        print("testing")
+        print("\nCompleted all cycles, generating summary...")
+        self.printSummary()
 
 
 
 if __name__ == '__main__':
     link_ls = ['https://en.wikipedia.org/wiki/Special:Random'] #wikipedia's random article link
-    main(link_ls, 5)
+    Crawler("links.txt", 3).crawl(link_ls)
     print("Complete")
